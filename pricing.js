9888ab1333c43356b5ff810872f6067cca32fdc3
tayfritz01@gmail.com <taylorfritz@Taylors-Air.attlocal.net>
2021-06-19 15:58:54 -0400

    pricing and dom selection

--- pricing.js
+++ pricing.js
@@ -1,4 +1,4 @@
-// Cookie package options - ADMIN
+// Cookie package options - ADMIN 
 // const adminPackageOptions = document.getElementById("admin-package-options");
 const packagePrices = [];
 const updatePrices = document.getElementById('update-prices');
@@ -19,15 +19,26 @@ basic.appendChild(basicInput);
 detailed.appendChild(detailedInput);
 elaborate.appendChild(elaborateInput);
 
+// Cookie package options - CUSTOMER PAGE
+
+const customerPackagePricing = document.getElementById('customer-package-options');
 
 updatePrices.addEventListener('click', () => {
     packagePrices.push(basicInput.valueAsNumber);
+    const basicLi = document.createElement("li");
+    basicLi.textContent = `Basic Package Price: $${basicInput.value}`
+    customerPackagePricing.appendChild(basicLi);
+    basicInput.value = "";
+
     packagePrices.push(detailedInput.valueAsNumber);
+    const detailedLi = document.createElement("li");
+    detailedLi.textContent = `Detailed Package Price: $${detailedInput.value}`
+    customerPackagePricing.appendChild(detailedLi);
+    detailedInput.value = "";
+
     packagePrices.push(elaborateInput.valueAsNumber);
+    const elaborateLi = document.createElement("li");
+    elaborateLi.textContent = `Elaborate Package Price: $${elaborateInput.value}`
+    customerPackagePricing.appendChild(elaborateLi);
+    elaborateInput.value = "";
 });
-
-// Cookie package options - CUSTOMER
-
-// const customerPackagePricing = document.getElementById('customer-package-options');
-// const li = document.createElement("li");
-// customerPackagePricing.appendChild(li);
