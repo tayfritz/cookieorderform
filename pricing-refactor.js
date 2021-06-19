34a253d457891f2cc81cc26093917f1587d9c323
tayfritz01@gmail.com <taylorfritz@Taylors-Air.attlocal.net>
2021-06-19 17:42:01 -0400

    order confirmation work

--- pricing-refactor.js
+++ pricing-refactor.js
@@ -5,9 +5,6 @@ const updatePrices = document.getElementById('update-prices');
 const basic = document.getElementById('basic-cookies');
 const detailed = document.getElementById('detailed-cookies');
 const elaborate = document.getElementById('elaborate-cookies');
-const customerPackagePricing = document.getElementById('customer-package-options');
-
-
 
 const basicInput = document.createElement('input');
 basicInput.type = 'number'
@@ -22,13 +19,15 @@ basic.appendChild(basicInput);
 detailed.appendChild(detailedInput);
 elaborate.appendChild(elaborateInput);
 
+const customerPackagePricing = document.getElementById('customer-package-options');
+
 updatePrices.addEventListener('click', () => {
     packagePrices.push(basicInput.valueAsNumber);
     const basicLi = document.createElement("li");
     basicLi.textContent = `Basic Package Price: $${basicInput.value}`
     customerPackagePricing.appendChild(basicLi);
     basicInput.value = "";
-    
+
     packagePrices.push(detailedInput.valueAsNumber);
     const detailedLi = document.createElement("li");
     detailedLi.textContent = `Detailed Package Price: $${detailedInput.value}`
@@ -41,3 +40,5 @@ updatePrices.addEventListener('click', () => {
     customerPackagePricing.appendChild(elaborateLi);
     elaborateInput.value = "";
 });
+
+// Cookie package options - CUSTOMER PAGE
