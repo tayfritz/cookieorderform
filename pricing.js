db5858530ac73f2124d89c23db899ca53354c665
tayfritz01@gmail.com <taylorfritz@Taylors-Air.attlocal.net>
2021-06-19 15:48:00 -0400

    Beginning work of admin input fields and js organization

--- pricing.js
+++ pricing.js
@@ -1,2 +1,33 @@
-// Cookie pricing info
+// Cookie package options - ADMIN
+// const adminPackageOptions = document.getElementById("admin-package-options");
+const packagePrices = [];
+const updatePrices = document.getElementById('update-prices');
+const basic = document.getElementById('basic-cookies');
+const detailed = document.getElementById('detailed-cookies');
+const elaborate = document.getElementById('elaborate-cookies');
 
+const basicInput = document.createElement('input');
+basicInput.type = 'number'
+
+const detailedInput = document.createElement('input');
+detailedInput.type = 'number'
+
+const elaborateInput = document.createElement('input');
+elaborateInput.type = 'number'
+
+basic.appendChild(basicInput);
+detailed.appendChild(detailedInput);
+elaborate.appendChild(elaborateInput);
+
+
+updatePrices.addEventListener('click', () => {
+    packagePrices.push(basicInput.valueAsNumber);
+    packagePrices.push(detailedInput.valueAsNumber);
+    packagePrices.push(elaborateInput.valueAsNumber);
+});
+
+// Cookie package options - CUSTOMER
+
+// const customerPackagePricing = document.getElementById('customer-package-options');
+// const li = document.createElement("li");
+// customerPackagePricing.appendChild(li);
