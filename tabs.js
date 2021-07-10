6a764211846e4b7556abee65f6d6a57a83581a1a
tayfritz01@gmail.com <taylorfritz@Taylors-Air.attlocal.net>
2021-07-10 18:14:33 -0400

    tabs working

--- tabs.js
+++ tabs.js
@@ -1,12 +1,23 @@
 const adminDiv = document.getElementById('admin-facing');
 const customerDiv = document.getElementById('customer-facing');
-const contentDivs = document.querySelectorAll('.tab-content');
 const tabLinks = document.querySelectorAll('.tablink');
+const contentDivs = document.getElementsByClassName('.content');
 
-
-function toggleTabs(event, divToDisplay, divToHide) {
-    divToHide.style.display = 'none';
-    divToDisplay.className = divToDisplay.className.replace('tab-content', 'active');``
+// Add an active class name to the div targeted
+// Remove 'active' class name on div that is not targeted
+function toggleTabs(event, addClassName) {
+    const buttonClicked = event.target;
+    if (buttonClicked.classList.contains('admin-btn')) {
+        if (customerDiv.classList.contains(addClassName)) {
+            customerDiv.classList.remove(addClassName);
+            adminDiv.classList.add(addClassName)
+        };
+    } else if (buttonClicked.classList.contains('customer-btn')) {
+        if (adminDiv.classList.contains(addClassName)) {
+            adminDiv.classList.remove(addClassName);
+            customerDiv.classList.add(addClassName)
+        };
+    }
 }
 
 
