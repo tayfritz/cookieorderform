213bf7911a72f38f13cf29960998bba1191efd58
Taylor Fritz <tayfritz01gmail.com>
2021-07-26 19:13:08 -0400

    validation on order form

--- src/order-confirmation.js
+++ src/order-confirmation.js
@@ -1,21 +1,79 @@
-// Order confirmation : CUSTOMER PAGE
+// Order Information & Customer | CUSTOMER PAGE
 const placeOrder = document.getElementById('place-order');
-const orderConfirmation = document.getElementById('order-confirmation');
+const custAlertDiv = document.getElementById('cust-alert-div');
 const custName = document.getElementById('cust-name');
-const custPhone = document.getElementById('cust-phone-number');
 const custEmail = document.getElementById('cust-email');
-const eventName = document.getElementById('event-name');
-const eventDate = document.getElementById('event-date');
-const dozens = document.getElementById('dozens');
-// const packageSelected = document.querySelectorAll("#cookie-package");
+const custPhone = document.getElementById('cust-phone-number');
+const custEventName = document.getElementById('event-name');
+const custEventDate = document.querySelector('#flatpickr');
+const custDozens = document.getElementById('dozens');
+const custPackageSelected = document.querySelectorAll("#cookie-package input");
+const confirmationDiv = document.getElementById('order-confirmation');
+confirmationDiv.style.display = 'none';
+custAlertDiv.style.display = 'none';
+const orders = []
 
-placeOrder.addEventListener('click', () => {
-   const newOrderObj = {
+// FUNCTIONS
+
+function  validateOrderInfo() {
+    // Validate name
+    if (custName.value === "") {
+        custAlertDiv.innerHTML = `<p> You must provide a name to place an order! </p>`
+        custAlertDiv.style.display = 'block';
+    } 
+
+}
+
+
+// function determinePickupDate(customerEventDate) {
+    
+// }
+
+
+// function determineOrderTotal(packageChosen, dozensOrdered) {
+
+// }
+
+
+// EVENT LISTENERS
+placeOrder.addEventListener('click', (e) => {
+    validateOrderInfo();
+    const newOrder = {
        name: `${custName.value}`,
        email: `${custEmail.value}`,
-       phone: `${custPhone.value}`
+       phone: `${custPhone.value}`,
+       eventName: `${custEventName.value}`,
+       eventDate: `${custEventDate.value}`,
+    //    pickupDate: determinePickupDate(custEventDate.value),
+    //    dozensNeeded: `${custDozens.value}`,
+    //    packageSelected: `${custPackageSelected.checked}`,
+    //    orderTotal: determineOrderTotal(custPackageSelected.checked, custDozens.value)
    }
+
+   let p = new Promise((resolve, reject) => {
+    let pushNewOrder = orders.push(newOrder);
+    if (pushNewOrder) {
+        resolve('Success! We received your order!');
+    } else {
+        reject('Something went wrong with your order. Please try again later.');
+    }
+   });
+
+   p.then((message) => {
+       console.log(message);
+   }).catch((message) => {
+        console.log('This is in the catch' + message);
+   });
 });
+
+// NOTES:
+// Validate inputs
+// If all are 'true', make a new object
+// Calculate pick up time based on event date
+// Calculate order total based on package chosen and price set
+// Print confirmation to customer
+// Push object to HTML table on ADMIN portal
+
 
 
 
