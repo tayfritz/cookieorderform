9fc2e87f2d2040639e55e2f35015fbf40973e601
Taylor Fritz <tayfritz01gmail.com>
2021-07-25 21:25:32 -0400

    refactor to card divs and creation of orders.js

---
+++ src/orders.js
@@ -0,0 +1,12 @@
+const orders = [
+    {
+        name: null,
+        email: null,
+        phone: null,
+        eventName: null,
+        eventDate: null,
+        dozensNeeded: null,
+        package: null,
+        inspo: null
+    }
+]
--- src/pricing-refactor.js
+++ src/pricing-refactor.js
@@ -5,9 +5,9 @@ const cookiePackages = [
     {
         packageName: 'BASIC',
         whiteColor: true,
-        metalic: false,
+        metallic: false,
         additionalColors: 2,
-        maxDesigns: 3,
+        maxDesigns: 2,
         dozenMinimum: .5,
         pricePerDozen: 0
     },
@@ -16,7 +16,7 @@ const cookiePackages = [
         whiteColor: true,
         metallic: true,
         additionalColors: 4,
-        maxDesigns: 4,
+        maxDesigns: 3,
         dozenMinimum: 1,
         pricePerDozen: 0
     },
@@ -57,9 +57,56 @@ priceInputsAll.push(inputBasic);
 priceInputsAll.push(inputDetailed);
 priceInputsAll.push(inputElaborate);
 
-const priceRegex = /^(\d+\.)(\d{2})$/
+// Place Package Info on CUSTOMER portal
+const customerPackageOptions = document.getElementById('customer-package-options');
+const pricesTbdCard = document.getElementById('prices-tbd');
+function summarizeColors(package) {
+    let totalColors;
+    if (package.whiteColor) {
+        totalColors = `white + ${package.additionalColors} additional colors of choice`;
+    } else {
+        totalColors = `${package.additionalColors} colors of choice`;
+    }
+    return totalColors;
+}
+function summarizeMetalics(package) {
+    let metalics;
+    if (package.metallic) {
+        metalics = `metalics`;
+    } else {
+        metalics = `metalics not included`;
+    }
+    return metalics;
+}
 
+function displayToCustomer(package, tally) {
+    let card;
+    let packageP;
+    let pricingP;
+    pricesTbdCard.style.display = 'none';
+    package.forEach((package) => {
+        card = document.createElement('div');
+        card.className = package.packageName;
+        card.innerHTML = `<p>${package.packageName}<p>`
+        packageP = document.createElement('p');
+        packageP.innerHTML = `<p>Included in package: ${summarizeColors(package)}, ${summarizeMetalics(package)}, ${package.maxDesigns} designs </p>`
+        customerPackageOptions.appendChild(card);
+        card.appendChild(packageP);
+
+        pricingP = document.createElement('p');
+        if (package.pricePerDozen == 0) {
+            pricingP.innerHTML = `Price per Dozen: TBD <br> Please contact shop for pricing details.`
+        } else {
+            pricingP.innerHTML = `Price per Dozen: $${package.pricePerDozen}`
+        }
+        card.appendChild(pricingP);
+    });
+}
+
+let tally = 0
+const priceRegex = /^(\d+\.)(\d{2})$/
 updatePriceButton.addEventListener('click', () => {
+    tally =+ 1;
     let counter = 0;
     for (let i=0; i<priceInputsAll.length; i++) {
         let inputValue = priceInputsAll[i].value;
@@ -70,61 +117,21 @@ updatePriceButton.addEventListener('click', () => {
         }
     }   
     if (counter > 0) {
-        alertAdminDiv.innerHTML = `<p>We had a problem with your prices. Here's what we got from you. If you'd like to try again, resubmit your prices. If not, any package marked as $0 will prompt the customer to reach out to you!</p>`
+        alertAdminDiv.innerHTML = `<p>We had a problem with your prices. Here's what we got from you. Please resubmit your prices so they can be updated in the customer portal!</p>`
         cookiePackages.forEach( (package) => {
             let result = `<br>${package.packageName} | $${package.pricePerDozen}`
             alertAdminDiv.insertAdjacentHTML('beforeend', result);
-        });  
+        });
     } else {
         alertAdminDiv.innerHTML = `<p>SUCCESS! Your prices have been updated! Here's what we got from you!</p>`
         cookiePackages.forEach( (package) => {
             let result = `<br>${package.packageName} | $${package.pricePerDozen}`
             alertAdminDiv.insertAdjacentHTML('beforeend', result);
         });
+        displayToCustomer(cookiePackages);
     }
     inputBasic.value= "";
     inputDetailed.value= "";
     inputElaborate.value= "";
 });
-
-// Place Updated Prices on CUSTOMER portal
-const customerPackageOptions = document.getElementById('customer-package-options');
-function summarizeColors(package) {
-    let totalColors;
-    if (package.whiteColor) {
-        totalColors = `white + ${package.additionalColors} additional colors of choice`;
-    } else {
-        totalColors = `${package.additionalColors} colors of choice`;
-    }
-    return totalColors;
-}
-
-function summarizeMetalics(package) {
-    let metalics;
-    if (package.metalic) {
-        metalics = `metalics`;
-    } else {
-        metalics = `metalics not included`;
-    }
-    return metalics;
-}
-
-cookiePackages.forEach((package) => {
-    const card = document.createElement('div');
-    card.className = package.packageName;
-    card.innerHTML = `<p>${package.packageName}<p>`
-    const p = document.createElement('p');
-    p.innerHTML = `<p>Included in package: ${summarizeColors(package)}, ${summarizeMetalics(package)}, ${package.maxDesigns} designs </p>`
-    customerPackageOptions.appendChild(card);
-    card.appendChild(p);
-});
-
-
-
-
-
-
-
-
-
 
